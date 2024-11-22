07cacc92da8bd5a9bfc88a6067e495c5d670b52a
Shepherd Yaw Morttey <shepherd@mfidie.com>
2024-11-22 03:23:03 +0000

    fixed minor slug edge case

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,31 @@
 class Item < ApplicationRecord
   extend FriendlyId
-  friendly_id :name, use: :slugged
+  friendly_id :slug_candidates, use: :slugged
 
   belongs_to :category
   has_many :prices, dependent: :destroy
   has_many :saved_items, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   validates :measurement, presence: true
+
+  private
+
+  def slug_candidates
+    if ["Phones & Tablets", "Laptops", "Electronics", "Vehicles"].include?(category&.name)
+      [
+        [:name, :measurement],
+        [:name, :measurement, :id]
+      ]
+    else
+      [
+        :name,
+        [:name, :id]
+      ]
+    end
+  end
+
+  def should_generate_new_friendly_id?
+    name_changed? || measurement_changed? || super
+  end
 end
